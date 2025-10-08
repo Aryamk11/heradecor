c97156d431bce6e9097231c3e135cd084bd6e716
Arya <arya.re2re2@gmail.com>
2025-10-08 16:47:48 +0330

    sec

--- src/assets/js/supabaseClient.js
+++ src/assets/js/supabaseClient.js
@@ -1,7 +1,8 @@
 // src/assets/js/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eqktbcdigrfgaqnindmm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxa3RiY2RpZ3JmZ2FxbmluZG1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMDEzMjQsImV4cCI6MjA3MDg3NzMyNH0.khk0abbnloMHidmNXvCFNRXdOpOXvs7UMQJn3EssKOI';
+// Access environment variables from .env file
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
